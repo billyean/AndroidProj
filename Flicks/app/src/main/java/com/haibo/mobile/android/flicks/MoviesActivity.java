a852897f3fe310ac554347a0a15af0eeb39e1558
billyean <tristan.yim@gmail.com>
2017-09-15 22:37:11 -0700

    Inital commit for Flicks

--- Flicks/app/src/main/java/com/haibo/mobile/android/flicks/MoviesActivity.java
+++ Flicks/app/src/main/java/com/haibo/mobile/android/flicks/MoviesActivity.java
@@ -3,11 +3,34 @@ package com.haibo.mobile.android.flicks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 public class MoviesActivity extends AppCompatActivity {
+    private static String SAMPLE_API_URL
+            = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(this, SAMPLE_API_URL, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
     }
 }
--- Flicks/app/src/main/java/com/haibo/mobile/android/flicks/model/Movie.java
+++ Flicks/app/src/main/java/com/haibo/mobile/android/flicks/model/Movie.java
@@ -5,4 +5,28 @@ package com.haibo.mobile.android.flicks.model;
  */
 
 public class Movie {
+    private final String posterPath;
+
+    private final String title;
+
+    private final String overview;
+
+
+    public Movie(String posterPath, String title, String overview) {
+        this.posterPath = posterPath;
+        this.title = title;
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
 }
